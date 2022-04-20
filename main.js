c0c028924a1725ce9e4cc029ab1969e93bc2655b
Brian Sitati <briansit93@gmail.com>
2022-04-20 20:18:37 +0300

    added a script for Google Books API

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var item, tile, author, publisher, bookLink, bookImg;
+    var outputList = document.getElementById("list-output");
+    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+    var placeHldr = '<img src="https://via.placeholder.com/150">';
+    var searchData;
+    
